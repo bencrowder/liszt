803945d8bfa88e4a00c82afcda5c8cbadb49d677
bencrowder <ben.crowder@gmail.com>
2016-03-08 12:31:24 -0700

    Adds unstar management command

---
+++ liszt/management/commands/unstar.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+
+from liszt.models import Context, List, Item, Tag
+from liszt.utils import process_payload
+
+import sqlite3
+
+class Command(BaseCommand):
+    args = '<list file>'
+    help = 'Unstars anything that is starred'
+
+    def handle(self, *args, **options):
+        try:
+            starred = Item.objects.filter(starred=True)
+
+            for item in starred:
+                item.starred = False
+                item.save()
+        except Exception as e:
+            print(e)
