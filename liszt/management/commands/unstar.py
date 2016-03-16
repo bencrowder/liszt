a1af09fb8bd986581fbcaa689acb87976ec6d442
bencrowder <ben.crowder@gmail.com>
2016-03-16 13:11:50 -0600

    Fixes unstar bug

--- liszt/management/commands/unstar.py
+++ liszt/management/commands/unstar.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 
-from liszt.models import Context, List, Item, Tag
+from liszt.models import Context, List, Item
 from liszt.utils import process_payload
 
 import sqlite3
