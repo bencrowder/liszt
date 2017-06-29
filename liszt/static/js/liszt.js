66512d088f1e22fbc4a282ffc422560141cafc91
bencrowder <ben.crowder@gmail.com>
2017-06-29 08:00:54 -0600

    Zoom hacks

--- liszt/static/js/liszt.js
+++ liszt/static/js/liszt.js
@@ -730,6 +730,22 @@ $(document).ready(function() {
 			}
 		});
 	}
+
+
+	// Hack for iOS 10 to prevent pinch-and-zoom
+	document.addEventListener('touchmove', function (event) {
+		if (event.scale !== 1) { event.preventDefault(); }
+	}, false);
+
+	// Hack for iOS 10 to prevent double-tap-and-zoom
+	var lastTouchEnd = 0;
+	document.addEventListener('touchend', function (event) {
+		var now = (new Date()).getTime();
+		if (now - lastTouchEnd <= 300) {
+			event.preventDefault();
+		}
+		lastTouchEnd = now;
+	}, false);
 });
 
 
